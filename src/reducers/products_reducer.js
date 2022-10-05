4283d81e8b3685e30b5476a67f343e0098263d2c
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-05 22:29:33 +0300

    Create Sidebar functionality

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -10,7 +10,15 @@ import {
 } from '../actions'
 
 const products_reducer = (state, action) => {
-  return state
+  if (action.type === SIDEBAR_OPEN) {
+    console.log(state)
+    return { ...state, isSidebarOpen: true }
+  }
+  if (action.type === SIDEBAR_CLOSE) {
+    console.log(state)
+    return { ...state, isSidebarOpen: false }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
