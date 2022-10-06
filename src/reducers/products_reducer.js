a82bc3e9dd6370c5f5629206a0c246c04b59c52b
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-06 17:19:30 +0300

    Complete products_context

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -18,6 +18,23 @@ const products_reducer = (state, action) => {
     console.log(state)
     return { ...state, isSidebarOpen: false }
   }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true }
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    )
+    return {
+      ...state,
+      products_loading: false,
+      products: action.payload,
+      featured_products,
+    }
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_error: true, products_loading: false }
+  }
 
   throw new Error(`No Matching "${action.type}" - action type`)
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,22 +23,19 @@ export const services = [
     id: 1,
     icon: <GiCompass />,
     title: 'mission',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
   {
     id: 2,
     icon: <GiDiamondHard />,
     title: 'vision',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
   {
     id: 3,
     icon: <GiStabbedNote />,
     title: 'history',
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
 ]
 
