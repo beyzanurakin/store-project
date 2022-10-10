b07bc96250b8fb6e46f3a8bfbd9f99322f08df22
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-10 21:50:52 +0300

    Create products context

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -15,7 +15,79 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 const SingleProductPage = () => {
-  return <h4>single product page</h4>
+  const { id } = useParams()
+  const history = useHistory()
+  console.log(id)
+  const {
+    single_product_loading: loading,
+    single_product_error: error,
+    single_product: product,
+    fetchSingleProduct,
+  } = useProductsContext()
+
+  useEffect(() => {
+    fetchSingleProduct(`${url}${id}`)
+    console.log(product)
+  }, [id])
+
+  //if there is en error back to homepage within 3 seconds
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 3000)
+    }
+  }, [error])
+  if (loading) {
+    return <Loading />
+  }
+  if (error) {
+    return <Error />
+  }
+  console.log(product)
+
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className='section section-center page'>
+        <Link to='/products'>back to products</Link>
+        <div className='product-center'>
+          <ProductImages images={images} />
+          <section className='content'>
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className='price'>{formatPrice(price)}</h5>
+            <p className='info'>
+              <span>Avalable : </span>
+              {stock > 0 ? 'In Stock' : 'out of stock'}
+            </p>
+            <p className='info'>
+              <span>SKU : </span>
+              {sku}
+            </p>
+            <p className='info'>
+              <span>Brand : </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.main`
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -35,6 +35,23 @@ const products_reducer = (state, action) => {
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_error: true, products_loading: false }
   }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return { ...state, single_product_loading: true }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    }
+  }
 
   throw new Error(`No Matching "${action.type}" - action type`)
 }
