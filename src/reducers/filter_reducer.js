744979ad8948aa35b4d00aa9d86a484fcb934e1a
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-13 14:02:10 +0300

    Add view buttons

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -19,6 +19,18 @@ const filter_reducer = (state, action) => {
       filtered_products: [...action.payload],
     }
   }
+  if (action.type === SET_GRIDVIEW) {
+    return {
+      ...state,
+      grid_view: true,
+    }
+  }
+  if (action.type === SET_LISTVIEW) {
+    return {
+      ...state,
+      grid_view: false,
+    }
+  }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
