46fc28773bd81326586bc40ac1ae74039a435f32
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-18 17:48:48 +0300

    Edit updateFilter func

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -63,6 +63,7 @@ const filter_reducer = (state, action) => {
     return { ...state, filters: { ...state.filters, [name]: value } }
   }
   if (action.type === FILTER_PRODUCTS) {
+    console.log('filtering')
     return { ...state }
   }
   return state
