ee98d40590b06f68253359c3b8d74c92cd8c4c97
beyzanurakin <akinbeyzanur@gmail.com>
2022-10-18 18:25:53 +0300

    Add price filter

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -115,6 +115,19 @@ const Filters = () => {
               })}
             </div>
           </div>
+          {/* Price */}
+          <div className='form-control'>
+            <h5>price</h5>
+            <p className='price'>{formatPrice(price)}</p>
+            <input
+              type='range'
+              name='price'
+              min={min_price}
+              max={max_price}
+              onChange={updateFilters}
+              value={price}
+            />
+          </div>
         </form>
       </div>
     </Wrapper>
--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -65,6 +65,9 @@ export const FilterProvider = ({ children }) => {
     if (name === 'color') {
       value = e.target.dataset.color
     }
+    if (name === 'price') {
+      value = Number(value)
+    }
     console.log(name, value)
     dispatch({ type: UPDATE_FILTERS, payload: { name, value } })
   }
