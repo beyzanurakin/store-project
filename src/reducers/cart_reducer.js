9887ccbcf312f1ac512c83699532dec6b94fc652
beyzanurakin <akinbeyzanur@gmail.com>
2022-11-01 22:37:30 +0300

    Create reducer function to manage adding item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -7,6 +7,37 @@ import {
 } from '../actions'
 
 const cart_reducer = (state, action) => {
+  if (action.type === ADD_TO_CART) {
+    const { id, color, amount, product } = action.payload
+    //checking if the product is in the cart now
+    const tempItem = state.cart.find((i) => i.id === id + color)
+    if (tempItem) {
+      const tempCart = state.cart.map((cartItem) => {
+        if (cartItem.id === id + color) {
+          let newAmount = cartItem.amount + amount
+          if (newAmount > cartItem.max) {
+            newAmount = cartItem.max
+          }
+          return { ...cartItem, amount: newAmount }
+        } else {
+          return cartItem
+        }
+      })
+      return { ...state, cart: tempCart }
+      //if the product is added for the first time
+    } else {
+      const newItem = {
+        id: id + color,
+        name: product.name,
+        color,
+        amount,
+        image: product.images[0].url,
+        price: product.price,
+        max: product.stock,
+      }
+      return { ...state, cart: [...state.cart, newItem] }
+    }
+  }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
